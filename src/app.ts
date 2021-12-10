c9a05ae5cdce71f9ece15e5b7e363f97bc8d38e5
Toxic254 <cyrus_g@outlook.com>
2021-12-10 19:00:55 +0300

    Created our base API application that communicates with JSON. With a connection to our postgres DB and our routes.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-
+import { createConnection } from 'typeorm';
+import { userRoute } from './router/user_routes';
 
 const app = express();
 
-app.listen(3000, () => {
+app.use(express.json());
+
+app.use('/user', userRoute);
+
+app.listen(3000, async () => {
+    await createConnection();
     return console.log(`Server started on PORT 3000.`);
 });
